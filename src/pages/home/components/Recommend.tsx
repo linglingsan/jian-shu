c3325e1b0a1c9c0258d678b55645714837b7dcc5
linglingsan <zhangchunlin5614@163.com>
2019-05-15 23:56:00 +0800

    右侧

--- src/pages/home/components/Recommend.tsx
+++ src/pages/home/components/Recommend.tsx
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { RecommendWrapper, RecommendItem } from '../style';
 
-
-class Recommend extends Component {
+class Recommend extends Component<any> {
     render() {
+        console.log(this.props.list);
         return (
-            <div>
-                Recommend~
-            </div>
-        );
+            <RecommendWrapper>
+                {
+                    this.props.list.map((item: any) => {
+                        return <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')}/>
+                    })
+                }
+            </RecommendWrapper>
+        )
     }
 }
 
-export default Recommend
+const mapState = (state: any) => ({
+    list: state.getIn(['home', 'recommendList'])
+})
+
+export default connect(mapState, null)(Recommend);
