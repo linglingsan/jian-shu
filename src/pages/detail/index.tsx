78b32b2acbd09f060bb6dca5b9499b7ba47030ad
linglingsan <zhangchunlin5614@163.com>
2019-05-17 23:57:37 +0800

    detail

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-
+import { connect } from 'react-redux';
+import { DetailWrapper, Header, Content } from './style';
 class Detail extends Component {
     render() {
         return (
-            <div>
+            <DetailWrapper>
                 Detail
-            </div>
+            </DetailWrapper>
         );
     }
 }
 
-export default Detail
+export default connect()(Detail)
